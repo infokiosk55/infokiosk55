afe2a2592f8ee34f32784142def1d127ee7286f7
infokiosk55 <110535476+infokiosk55@users.noreply.github.com>
2022-08-04 18:13:22 +0600

    test2

---
+++ src/pages/BikePathsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**Функция, отвечающая за вывод страницы с велосипедными дорожками
+ * @author Kolisratova Yuliya
+ * @constructor
+ */
+export function BikePathsPage(){
+    return(
+        <div>
+            Велосипедные дорожки
+        </div>
+    );
+}
---
+++ src/pages/PhotoshootPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**Функция, отвечающая за вывод страницы с местами для фотосессий
+ * @author Kolisratova Yuliya
+ * @constructor
+ */
+export function PhotoshootPage(){
+    return(
+        <div>
+            Места для фотосессий
+        </div>
+    );
+}
