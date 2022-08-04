2b14ded44b96ac294a0881e56d8f822c3f1d0126
infokiosk55 <110535476+infokiosk55@users.noreply.github.com>
2022-08-04 16:46:03 +0600

    test

---
+++ src/pages/EventPosterPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**Функция, отвечающая за вывод страницы с афишей событий в городе
+ * @author Kolisratova Yuliya
+ * @constructor
+ */
+export function EventPosterPage(){
+    return(
+        <div>
+            Мероприятие
+        </div>
+    );
+}
---
+++ src/pages/TheatersAndMuseumsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**Функция, отвечающая за вывод страницы с театрами и музеями
+ * @author Kolisratova Yuliya
+ * @constructor
+ */
+export function TheatersAndMuseumsPage(){
+    return(
+        <div>
+            Театры и музеи
+        </div>
+    );
+}
